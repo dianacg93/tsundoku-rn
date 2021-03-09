99e4f0e2a744d4cfc3635692b5cba06f200f52d5
Shafee <shafeelinks@gmail.com>
2021-03-09 16:10:48 -0500

    boilerplate added and dependencies

---
+++ src/Screens/BestSellers.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const BestSellers = () => {
+	return (
+		<View>
+			<Text>Best sellers Screen</Text>
+		</View>
+	);
+};
+
+export default BestSellers;
+
+const styles = StyleSheet.create({});
---
+++ src/Screens/LocalShopsSources.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const LocalShopsSources = () => {
+	return (
+		<View>
+			<Text>Local Shop Sources</Text>
+		</View>
+	);
+};
+
+export default LocalShopsSources;
+
+const styles = StyleSheet.create({});
