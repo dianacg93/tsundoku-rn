746bf5ddd087520487202f2c186541a80e51f44c
Diana <diana.cg93@gmail.com>
2021-03-23 14:26:07 -0400

    API Screens
    Get bestsellers dropdown
    Get search to display quote or search results

---
+++ quoteData.js
@@ -0,0 +1,40 @@
+const quoteData= [
+    {
+        "quote": "It is our knowledge — the things we are sure of — that makes the world go wrong and keeps us from seeing and learning.",
+        "author": "Lincoln Steffens"
+    },
+    {
+        "quote": "If you only read the books that everyone else is reading, you can only think what everyone else is thinking.",
+        "author": "Haruki Murakami"
+    },
+    {
+        "quote": "One glance at a book and you hear the voice of another person, perhaps someone dead for 1,000 years. To read is to voyage through time.",
+        "author": "Carl Sagan"
+    },
+    {
+        "quote": "If you have a garden and a library, you have everything you need.",
+        "author": "Cicero"
+    },
+    {
+        "quote": "A successful book is not made of what is in it, but what is left out of it.",
+        "author": "Mark Twain"
+    },
+    {
+        "quote": "Be as careful of the books you read, as of the company you keep; for your habits and character will be as much influenced by the former as the latter.",
+        "author": "Paxton Hood"
+    },
+    {
+        "quote": "Books are the perfect entertainment: no commercials, no batteries, hours of enjoyment for each dollar spent. What I wonder is why everybody doesn't carry a book around for those inevitable dead spots in life.",
+        "author": "Stephen King"
+    },
+    {
+        "quote": "It is what you read when you don't have to that determines what you will be when you can't help it.",
+        "author": "Oscar Wilde"
+    },
+    {
+        "quote": "The world is a book and those who do not travel read only one page.",
+        "author": "St. Augustine"
+    },
+];
+
+export default quoteData;
--- src/screens/Bestsellers/BestSellersScreen.jsx
+++ src/screens/Bestsellers/BestSellersScreen.jsx
@@ -1,59 +1,79 @@
-import React, {useState, useEffect} from 'react'
-import {View, Text, StyleSheet, Button, Image} from 'react-native';
+import React, {useState, useEffect, useContext} from 'react'
+import {View, Text, StyleSheet, Image} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 import axios from 'axios';
 import config from '../../../config'
 
-const style = StyleSheet.create({
-    container: {flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'},
-    giphyImg: {
-        width:250,
-        height: 250
-    }
-})
-
 const BestsellersScreen = () => {
 
-    const [bestsellers, setBestsellers] = useState("");
-    const BASE_URL = `${config.REACT_APP_NYT_URL}api-key=${config.REACT_APP_NYT_API_KEY}`;
+    const [bestsellers, setBestsellers] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [topic, setTopic] = useState("travel");
 
-    const fetchBestsellers = async () => {
-        const res = await axios.get(BASE_URL);
-        setBestsellers(res.data.results);
-    }
+	const fetchCategoryNames = async () => {
+		const response = await axios.get(`${config.REACT_APP_NYT_CATEGORY_NAMES}api-key=${config.REACT_APP_NYT_API_KEY}`);
 
+		setCategories(response.data.results);
+	};
+
+	const renderTopicSection = async () => {
+		const response = await axios.get(`${config.REACT_APP_NYT_CATEGORY}${topic}.json?api-key=${config.REACT_APP_NYT_API_KEY}`);
+
+		setBestsellers(response.data.results.books);
+	};
+    
     useEffect(() => {
-        fetchBestsellers()
-    }, [])
-
-    const onPressChange = () => {
-        fetchBestsellers();
-    }
-
-    console.log(bestsellers)
+        fetchCategoryNames();
+    }, []);
+    
+	useEffect(() => {
+        renderTopicSection()
+    }, [topic]);
 
     return (
 
         <View style={style.container}>
             <Text>NYT Best Sellers</Text>
-            <Button 
-            title="Fetch"
-            onPress={onPressChange}
-             />
-            <View>
-                {bestsellers != "" ? bestsellers?.books.map(el => {
-                    return <View key={el.primary_isbn10}>
+            <Picker
+                selectedValue={topic}
+                onValueChange={(itemValue) =>
+                    setTopic(itemValue)
+                }
+                >
+                {categories.map((cat, i) => {
+                    return (
+                        <Picker.Item 
+                            key={i}
+                            label={cat.list_name} 
+                            value={cat.list_name_encoded}
+                        />
+                    )
+                })}
+            </Picker>
+            {bestsellers.length ? bestsellers?.map(el => {
+                return (
+                    <View key={el.primary_isbn10}>
                         <Text >{el.title}</Text>
-                        <Text>Here</Text>
                         <Image 
-                        style={style.giphyImg}
-                        source={{uri: `${el.book_image}`}}/>
+                            style={style.bestsellerImg}
+                            source={{uri: `${el.book_image}`}}/>
+                        <Text>by {el.title}</Text>
                     </View>
-                }) : ""}
-            </View>
+                )
+            }) : ""}
         </View>
     )
 }
+
+
+const style = StyleSheet.create({
+    container: {flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'},
+    bestsellerImg: {
+        width: 180,
+        height: 250
+    }
+})
 
 export default BestsellersScreen;
