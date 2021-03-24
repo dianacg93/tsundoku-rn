6978c157fdbb1e39a70f0d55385058b8d053577a
Shan Siddiqui <shansiddiqui@Shans-Air.lan>
2021-03-23 23:10:03 -0400

    AboutUs updated, made to look like the desktop page

--- src/screens/AboutUs/AboutUsScreen.jsx
+++ src/screens/AboutUs/AboutUsScreen.jsx
@@ -7,58 +7,63 @@ const AboutUsScreen = () => {
   return (
     <View style={styles.container}>
       <View>
-        <Text>Diana</Text>
-        <Text>Kitchen</Text>
-        <Text>Banana Yoshimoto</Text>
+        <Text style={styles.reads}>TEAM'S FAVORITE READS</Text>
+        <br />
+      </View>
+      <View style={styles.text}>
+        <Text style={styles.name}>DIANA GAONA</Text>
+        <Text style={styles.title}>Kitchen</Text>
+        <Text style={styles.spc}>by Banana Yoshimoto</Text>
         <Image
           source={{
             uri:
               "https://prodimage.images-bn.com/pimages/9780802142443_p0_v1_s600x595.jpg",
           }}
-          style={{ width: 40, height: 40 }}
+          style={{ width: 75, height: 90 }}
         />
-        <Text>
-          About me: Hello fellow readers! My name is Diana. Growing up reading
-          books in Spanish and eventually transitioning to reading books in
-          English, literature has been and continues to be a past time that
-          allows me to see into other people's minds and learn many lessons I
-          may have not learned in my day-to-day routine. As I have grown older,
-          I have seen my favorite genre change many times, and at the moment I
-          find myself reading self-help and business books
+        <Text style={styles.pad}>
+          Hello fellow readers! My name is Diana. Growing up reading books in
+          Spanish and eventually transitioning to reading books in English,
+          literature has been and continues to be a past time that allows me to
+          see into other people's minds and learn many lessons I may have not
+          learned in my day-to-day routine. As I have grown older, I have seen
+          my favorite genre change many times, and at the moment I find myself
+          reading self-help and business books
         </Text>
       </View>
 
-      <View>
-        <Text>Shafee</Text>
-        <Text>The Brothers Karamazov</Text>
-        <Text>Fyodor Dostoevsky</Text>
+      <View style={styles.text}>
+        <Text style={styles.name}>SHAFEE AHMED</Text>
+        <Text style={styles.title}>The Brothers Karamazov</Text>
+        <Text style={styles.spc}>by Fyodor Dostoevsky</Text>
         <Image
           source={{
             uri:
-              "https://prodimage.images-bn.com/pimages/9780140449242_p0_v2_s600x595.jpg",
+              "https://images-na.ssl-images-amazon.com/images/I/917QNDtEGML.jpg",
           }}
-          style={{ width: 40, height: 40 }}
+          style={{ width: 75, height: 90 }}
         ></Image>
-        <Text>
+        <Text style={styles.pad}>
           Hey, Shafee here. My love for literature started when I read a book by
           Haruki Murakami called 'Kafka on the Shore'. His works led me to one
           of my favorite genres of literature, Russian, with authors like
           Dostoevsky and Bulgakov.
         </Text>
+        <br />
       </View>
 
-      <View>
-        <Text>Jahaziel</Text>
-        <Text>Between the World and Me</Text>
-        <Text>Ta-Nehisi Coates</Text>
+      <View style={styles.text}>
+        <Text style={styles.name}>JAHAZIEL ISRAEL</Text>
+        <Text style={styles.title}>Between the World and Me</Text>
+        <Text style={styles.spc}>by Ta-Nehisi Coates</Text>
         <Image
           source={{
             uri:
               "https://prodimage.images-bn.com/pimages/9780812993547_p0_v5_s600x595.jpg",
           }}
-          style={{ width: 40, height: 40 }}
+          style={{ width: 75, height: 90 }}
         ></Image>
-        <Text>
+        <Text style={styles.pad}>
           Hi, I'm Jahaziel and when I'm not rummaging through the dozens of
           sidewalk carts of discounted books at Strand Bookstore or Center for
           Fiction in Brooklyn, you can certainly find me breaking all sorts of
@@ -66,25 +71,26 @@ const AboutUsScreen = () => {
           freetime writing hand-lettered calligraphy cards and letters to close
           friends.
         </Text>
+        <br />
       </View>
 
-      <View>
-        <Text>Shan</Text>
-        <Text>Guns, Germs, and Steel</Text>
-        <Text>Jared Diamond</Text>
+      <View style={styles.text}>
+        <Text style={styles.name}>SHAN SIDDIQUI</Text>
+        <Text style={styles.title}>Guns, Germs, and Steel</Text>
+        <Text style={styles.spc}> by Jared Diamond</Text>
         <Image
           source={{
             uri:
               "https://prodimage.images-bn.com/pimages/9780393354324_p0_v3_s600x595.jpg",
           }}
-          style={{ width: 40, height: 40 }}
+          style={{ width: 75, height: 90 }}
         ></Image>
-        <Text>
-          "Hey, I'm Shan and I have been to many places in books, some real,
-          some fictional. Most of them are far away. However I feel like I own a
+        <Text style={styles.pad}>
+          Hey, I'm Shan and I have been to many places in books, some real, some
+          fictional. Most of them are far away. However I feel like I own a
           piece of those places because of the fictional memories I have there.
           Books hold insight, so many of things I know about life I learned from
-          books. Books have shaped me as a person."
+          books. Books have shaped me as a person.
         </Text>
       </View>
     </View>
@@ -93,9 +99,43 @@ const AboutUsScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#FFFAF1",
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    padding: 20,
+  },
+  text: {
+    flex: 1,
+    justifyContent: "center",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    fontSize: 12,
+    padding: 20,
+    fontWeight: "bold",
+    borderColor: "#EBECF0",
+    borderWidth: 5,
+    borderTop: 2,
+  },
+  title: {
+    color: "#B91C1C",
+    fontWeight: "bold",
+  },
+  pad: {
+    padding: 10,
+    textAlign: "justify",
+  },
+  name: {
+    fontSize: 20,
+    paddingBottom: 10,
+  },
+  spc: {
+    paddingBottom: 10,
+  },
+  reads: {
+    fontSize: "30",
+    fontWeight: "bold",
   },
 });
 
